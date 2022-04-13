799ce775cca304c1a624d89bdb8c1df3c9cb06f5
SZENASI13 <91563696+SZENASI13@users.noreply.github.com>
2022-04-13 10:25:34 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,64 @@
+const kerdesek = [
+    {
+       id:1, 
+      question: "1. A Sikoly legelején Casey Becker (Drew Barrymore) mit süt éppen?",
+      answers: {
+        a: "Süteményt",
+        b: "Popcornt",
+        c: "Almás pitét",
+        d: "Steaket"
+      },
+      correctAnswer: "b",
+      foto:"scream1.jpg"
+    },
+    {
+       id:2, 
+      question: "Mi az ikonikus mondat, amit Ghostface, a gyilkos mindig megkérdez?",
+      answers: {
+        a: "„Tényleg bezártad a hátsó ajtót?...”",
+        b: "„Hogyan akarsz meghalni?”",
+        c: "„Mi a kedvenc horrorfilmed?”",
+        d: "„Szerinted kit nézek éppen?...”"
+      },
+      correctAnswer: "c",
+      foto:"scream2.jpg"
+    },
+    {
+       id:3, 
+      question: "A film végén, Stu partiján melyik horrorfilmet nézik közösen a tévében?",
+      answers: {
+        a: "Péntek 13",
+        b: "Halloween",
+        c: "Ördögűző",
+        d: "Döfés"
+      },
+      correctAnswer: "b",
+      foto:"scream3.jpg"
+    },
+
+    {
+      id:4, 
+     question: "A filmmogul Randy szerint mi a horrorfilmek/túlélés első szabálya?",
+     answers: {
+       a: "Ne szexelj!",
+       b: "Ne igyál és ne drogozz!",
+       c: "Sose mondd azt, hogy visszajövök!",
+       d: "Soha, de soha ne vedd fel a telefont!"
+     },
+     correctAnswer: "a",
+     foto:"scream4.jpg"
+   },
+
+   {
+    id:5, 
+   question: " A gyilkos szerint mit használtak művérként az első Sikoly legvégén?",
+   answers: {
+     a: "Piros festéket",
+     b: "Málnaszörpöt",
+     c: "Hígítót",
+     d: "Malacvért"
+   },
+   correctAnswer: "b",
+   foto:"scream5.jpg"
+ }
+  ];
